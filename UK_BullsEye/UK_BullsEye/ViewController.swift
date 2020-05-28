8cf21510f4fad79f565540cd8f60b64a872d3b50
altmurzilka <altmurzilka@gmail.com>
2020-05-28 22:18:56 +0600

    -

--- UK_BullsEye/UK_BullsEye/ViewController.swift
+++ UK_BullsEye/UK_BullsEye/ViewController.swift
@@ -30,7 +30,17 @@ class ViewController: UIViewController {
         slider.setThumbImage(thumbImageNormal, for: .normal)
         
         let thumbImageHighlited = #imageLiteral(resourceName: "SliderThumb-Highlighted")
-        slider.setThumbImage(thumbImageHighlited, for: .normal)
+        slider.setThumbImage(thumbImageHighlited, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
     }
     @IBAction func showAlert() {
         
