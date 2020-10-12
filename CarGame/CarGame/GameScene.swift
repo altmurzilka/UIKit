6b30d5775216e1b18b2fc9f5b01ffa916d02a90a
altyn <altmurzilka@gmail.com>
2020-10-13 01:00:52 +0600

    first part done works fine

---
+++ CarGame/CarGame/GameScene.swift
@@ -0,0 +1,66 @@
+//
+//  GameScene.swift
+//  CarGame
+//
+//  Created by Алтын on 10/12/20.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var leftCar = SKSpriteNode()
+    var rightCar = SKSpriteNode()
+    
+    override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        setUp()
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(GameScene.createRoadStrip), userInfo: nil, repeats: true)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        showRoadStrip()
+    }
+    
+    func setUp() {
+        leftCar = self.childNode(withName: "leftCar") as! SKSpriteNode
+        rightCar = self.childNode(withName: "rightCar") as! SKSpriteNode
+    }
+    
+    @objc func createRoadStrip() {
+        let leftRoadStrip = SKShapeNode(rectOf: CGSize(width: 10, height: 40))
+        leftRoadStrip.strokeColor = SKColor.white
+        leftRoadStrip.fillColor = SKColor.white
+        leftRoadStrip.alpha = 0.4
+        leftRoadStrip.name = "leftRoadStrip"
+        leftRoadStrip.zPosition = 10
+        leftRoadStrip.position.x = -187.5
+        leftRoadStrip.position.y = 700
+        addChild(leftRoadStrip)
+        
+        let rightRoadStrip = SKShapeNode(rectOf: CGSize(width: 10, height: 40))
+        rightRoadStrip.strokeColor = SKColor.white
+        rightRoadStrip.fillColor = SKColor.white
+        rightRoadStrip.alpha = 0.4
+        rightRoadStrip.name = "rightRoadStrip"
+        rightRoadStrip.zPosition = 10
+        rightRoadStrip.position.x = 187.5
+        rightRoadStrip.position.y = 700
+        addChild(rightRoadStrip)
+    }
+    
+    func showRoadStrip() {
+        enumerateChildNodes(withName: "leftRoadStrip") { (roadStrip, stop) in
+            let strip = roadStrip as! SKShapeNode
+            strip.position.y -= 30
+        }
+        
+        enumerateChildNodes(withName: "rightRoadStrip") { (roadStrip, stop) in
+            let strip = roadStrip as! SKShapeNode
+            strip.position.y -= 30
+        }
+    }
+    
+ 
+}
